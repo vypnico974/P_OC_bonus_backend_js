c16f6fcdd8503c4c48b7fde2e6748dd7f2cd978a
Vyplasil Nicolas <nicolas.vyplasil@hotmail.com>
2023-05-31 08:13:59 +0400

    mongoDB

---
+++ back/app.js
@@ -0,0 +1,75 @@
+// const express = require('express');
+
+// const app = express();
+
+// module.exports = app;
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://vypnicolas:Adl1L11Z0xR6oAMS@cluster0.czpfhrm.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+// app.use((req, res, next) => {
+//   console.log('Requête reçue !');
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.status(201);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.json({ message: 'Votre requête a bien été reçue !' });
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('Réponse envoyée avec succès !');
+// });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+});
+
+
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+
+module.exports = app;
