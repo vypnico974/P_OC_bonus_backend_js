b2239f0520f6974cceaf5e70b9c0919a781e5230
Vyplasil Nicolas <nicolas.vyplasil@hotmail.com>
2023-06-01 14:37:58 +0400

    authentification

--- back/app.js
+++ back/app.js
@@ -5,8 +5,14 @@
 // module.exports = app;
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
+const stuffRoutes = require('./routes/stuff');
+// const Thing = require('./models/thing');
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://vypnicolas:Adl1L11Z0xR6oAMS@cluster0.czpfhrm.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -14,7 +20,18 @@ mongoose.connect('mongodb+srv://vypnicolas:Adl1L11Z0xR6oAMS@cluster0.czpfhrm.mon
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+
+
 app.use(express.json());
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+app.use('/api/stuff', stuffRoutes);
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(bodyParser.json());
 
 // app.use((req, res, next) => {
 //   console.log('Requête reçue !');
@@ -42,34 +59,67 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
-    });
-});
+//app.use(bodyParser.json());
+
+// app.post('/api/stuff', (req, res, next) => {
+//     console.log(req.body);
+//     res.status(201).json({
+//       message: 'Objet créé !'
+//     });
+// });
+
+// app.put('/api/stuff/:id', (req, res, next) => {
+//     Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+//       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+//       .catch(error => res.status(400).json({  error  }));
+// });
+
+// app.post('/api/stuff', (req, res, next) => {
+//     delete req.body._id;
+//     const thing = new Thing({
+//       ...req.body
+//     });
+//     thing.save()
+//       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+//       .catch(error => res.status(400).json({ error }));
+// });
+
+// app.delete('/api/stuff/:id', (req, res, next) => {
+//     Thing.deleteOne({ _id: req.params.id })
+//       .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+//       .catch(error => res.status(400).json({ error }));
+// });
+
+// app.get('/api/stuff/:id', (req, res, next) => {
+//     Thing.findOne({ _id: req.params.id })
+//       .then(thing => res.status(200).json(thing))
+//       .catch(error => res.status(404).json({ error }));
+// });
 
 
-app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+// app.get('/api/stuff', (req, res, next) => {
+//     Thing.find()
+//     .then(things => res.status(200).json(things))
+//     .catch(error => res.status(400).json({ error }));
+//     // const stuff = [
+//     //   {
+//     //     _id: 'oeihfzeoi',
+//     //     title: 'Mon premier objet',
+//     //     description: 'Les infos de mon premier objet',
+//     //     imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//     //     price: 4900,
+//     //     userId: 'qsomihvqios',
+//     //   },
+//     //   {
+//     //     _id: 'oeihfzeomoihi',
+//     //     title: 'Mon deuxième objet',
+//     //     description: 'Les infos de mon deuxième objet',
+//     //     imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//     //     price: 2900,
+//     //     userId: 'qsomihvqios',
+//     //   },
+//     // ];
+//     // res.status(200).json(stuff);
+//   });
 
 module.exports = app;
---
+++ back/routes/stuff.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const stuffCtrl = require('../controllers/stuff');
+
+//router.get('/', auth, stuffCtrl.getAllThings);
+router.get('/', auth, stuffCtrl.getAllStuff);
+
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+
+module.exports = router;
+
+
+
+// const express = require('express');
+// const router = express.Router();
+
+// const stuffCtrl = require('../controllers/stuff');
+
+// router.get('/', stuffCtrl.getAllStuff);
+// router.post('/', stuffCtrl.createThing);
+// router.get('/:id', stuffCtrl.getOneThing);
+// router.put('/:id', stuffCtrl.modifyThing);
+// router.delete('/:id', stuffCtrl.deleteThing);
+
+// module.exports = router;
+
+
+// const express = require('express');
+// const router = express.Router();
+
+// const Thing = require('../models/thing');
+
+// router.post('/', (req, res, next) => {
+//   const thing = new Thing({
+//     title: req.body.title,
+//     description: req.body.description,
+//     imageUrl: req.body.imageUrl,
+//     price: req.body.price,
+//     userId: req.body.userId
+//   });
+//   thing.save().then(
+//     () => {
+//       res.status(201).json({
+//         message: 'Post saved successfully!'
+//       });
+//     }
+//   ).catch(
+//     (error) => {
+//       res.status(400).json({
+//         error: error
+//       });
+//     }
+//   );
+// });
+
+// router.get('/:id', (req, res, next) => {
+//   Thing.findOne({
+//     _id: req.params.id
+//   }).then(
+//     (thing) => {
+//       res.status(200).json(thing);
+//     }
+//   ).catch(
+//     (error) => {
+//       res.status(404).json({
+//         error: error
+//       });
+//     }
+//   );
+// });
+
+// router.put('/:id', (req, res, next) => {
+//   const thing = new Thing({
+//     _id: req.params.id,
+//     title: req.body.title,
+//     description: req.body.description,
+//     imageUrl: req.body.imageUrl,
+//     price: req.body.price,
+//     userId: req.body.userId
+//   });
+//   Thing.updateOne({_id: req.params.id}, thing).then(
+//     () => {
+//       res.status(201).json({
+//         message: 'Thing updated successfully!'
+//       });
+//     }
+//   ).catch(
+//     (error) => {
+//       res.status(400).json({
+//         error: error
+//       });
+//     }
+//   );
+// });
+
+// router.delete('/:id', (req, res, next) => {
+//   Thing.deleteOne({_id: req.params.id}).then(
+//     () => {
+//       res.status(200).json({
+//         message: 'Deleted!'
+//       });
+//     }
+//   ).catch(
+//     (error) => {
+//       res.status(400).json({
+//         error: error
+//       });
+//     }
+//   );
+// });
+
+// router.get('/' +
+//   '', (req, res, next) => {
+//   Thing.find().then(
+//     (things) => {
+//       res.status(200).json(things);
+//     }
+//   ).catch(
+//     (error) => {
+//       res.status(400).json({
+//         error: error
+//       });
+//     }
+//   );
+// });
+
+// module.exports = router;
